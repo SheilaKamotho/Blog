e48c48e9fea88ebdf79ccbe77e19d8672b47c00d
Kamotho Sheila <kamothosheila@gmail.com>
2020-09-28 17:28:27 +0300

    add auth changes

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,11 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 
-
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 
 def create_app(config_name):
-
+    
     app = Flask(__name__)
 
     # Creating the app configurations
@@ -17,13 +16,15 @@ def create_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
-
+    
     # Will add the views and forms
 
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-
+      
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
     # # setting config
     # # from .request import configure_request
     # configure_request(app)
